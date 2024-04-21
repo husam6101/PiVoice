5781125168c9e6c4610b71a004cfaf91aa6593bf
husam <husam6101@gmail.com>
2024-04-21 20:00:34 +0300

    added logs

--- pi_voice/main.py
+++ pi_voice/main.py
@@ -7,10 +7,12 @@ from pi_voice.operators.DataOperator import DataOperator
 from pi_voice.operators.LGBMOperator import LGBMOperator
 from pi_voice.switcher.ActionSwitcher import ActionSwitcher
 from pi_voice.switcher.SensorSwitcher import SensorSwitcher
+from pi_voice.operators import logger
 
 import threading
 import datetime
 import time
+import signal
 
 
 class MainProcess:
@@ -24,7 +26,7 @@ class MainProcess:
         self.lgbm = LGBMOperator()
 
     def record_audio(self):
-        print("Recording audio...")
+        logger.info("Recording audio...")
         recorded_file = self.audio_op.record_audio()
         return recorded_file
 
@@ -38,38 +40,43 @@ class MainProcess:
         return thread
 
     def _transcription_command_process(self, audio):
-        print(">> Started transcription-command process")
         # 1. process audio into transcript
+        logger.info("Transcribing audio...")
         transcript = self.whisper.process(audio)
-        print(">> Transcription: " + transcript)
+        logger.info("Done, transcription: " + transcript)
 
         # 2. process transcript into command
+        logger.info("Predicing action...")
         command = self.gpt.random_command(transcript)
-        print(">> Predicted command: " + command + ", activating command....")
+        logger.info("Done, predicted command: " + command)
 
         # 3. activate command
+        logger.info("Activating command: " + command)
         self.action_switcher.take_action(command)
-        print(">> Activated command.")
+        logger.info("Command activated.")
 
         # 4. get data from sensors and timestamps
+        logger.info("Fetching data from sensors...")
         temp, humid, light = self.sensor_switcher.get_data()
         time_of_day, day_of_week = self._get_ToD_and_DoW()
+        logger.info("Done.")
 
-        print(">> Writing data to lgbm dataset...")
+        logger.info("Data to be written to file:")
+        logger.info([humid, temp, light, command, time_of_day, day_of_week])
+
         # 5. save command in dataset for lgbm
+        logger.info("Writing data file...")
         writing_lgbm_data.acquire()
-        print(">> Data being added to CSV:")
-        print([temp, humid, light, time_of_day, day_of_week, command])
         self.data_op.add_row_to_csv(
             get_path_from(config["lgbm"]["dataset"]),
             [temp, humid, light, time_of_day, day_of_week, command]
         )
         writing_lgbm_data.release()
-        print(">> Finished writing data!")
+        logger.info("Done.")
 
-        print(">> Ended transcription-command process, waiting for thread break...")
+        logger.info("Waiting for thread break...")
         time.sleep(0.1)  # giving thread breaks
-        print(">> Thread break ended.")
+        logger.info("Thread break ended.")
 
     def _get_ToD_and_DoW(self):  # Time of Day and Day of Week
         current_date = datetime.datetime.now()
@@ -85,25 +92,51 @@ class MainProcess:
         return thread
 
     def _personalized_command_process(self):
-        print(">> Started personalized command process")
+        logger.info("Entered personalized-command process.")
         while True:
             writing_lgbm_data.acquire()
+            logger.info("Getting next target time...")
             target_time = self._get_next_notable_timestamp()
             writing_lgbm_data.release()
+            logger.info("Done. Target time is " + str(target_time))
 
             if target_time is None:
+                logger.info("Skipping...")
                 continue
 
+            logger.info("Calculating remaining time...")
             current_time = time.time()
             remaining_time = target_time - current_time
+            logger.info("Done. Remaining time is " + str(remaining_time))
 
             if target_time is None:
+                logger.info("Skipping...")
                 continue
 
             if remaining_time > 0:
+                logger.info("Waiting for " + str(remaining_time))
                 time.sleep(remaining_time)
 
+            logger.info("Training model...")
             self.lgbm.load_data_and_train_model()
+            logger.info("Finished training model.")
+            logger.info("Getting data...")
+            temp, humid, light = self.sensor_switcher.get_data()
+            time_of_day, day_of_week = self._get_ToD_and_DoW()
+            data_point = {
+                'Humidity (%)': humid,
+                'Temperature (C)': temp,
+                'Light Levels (lux)': 0,
+                'Time of Day': time_of_day,
+                'Day of Week': day_of_week
+            }
+            logger.info("Done.")
+            logger.info("Predicting with LightGBM...")
+            prediction = self.lgbm.predict(data_point)
+            logger.info("Done. Prediction: " + prediction)
+            logger.info("Activating action...")
+            self.action_switcher.take_action(prediction)
+            logger.info("Done.")
             time.sleep(61)
 
     def _get_next_notable_timestamp(self):
@@ -132,13 +165,15 @@ class MainProcess:
 
 
 def run():
-    print(">> Started main process")
+    logger.info(">> Started main process")
     process = MainProcess()
     action_switcher = ActionSwitcher()
     action_switcher.is_ready_device.on()
     try:
         process.main_thread().start()
-        # process.personalized_command_thread().start()
+        process.personalized_command_thread().start()
+        signal.signal(signal.SIGINT, lambda signum,
+                      frame: action_switcher.reset_all())
     except Exception:
         action_switcher.reset_all()
 
