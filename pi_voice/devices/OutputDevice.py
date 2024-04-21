3f32ca578c8200a52a4e8a094ab4bdd0244c3e32
husam <husam6101@gmail.com>
2024-04-21 21:07:45 +0300

    removed debug prints

--- pi_voice/devices/OutputDevice.py
+++ pi_voice/devices/OutputDevice.py
@@ -9,17 +9,14 @@ class OutputDevice:
         self.rpi_pin_interface = RPiPinInterface()
 
         self.gpio = self.rpi_pin_interface.get_gpios_for(device_name)[0]
-        print("GPIO for " + device_name + " is:")
         print(self.gpio)
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(self.gpio, GPIO.OUT)
 
     def on(self):
         GPIO.output(self.gpio, GPIO.HIGH)
-        print(">> Current GPIO state:")
         print(GPIO.input(self.gpio))
 
     def off(self):
         GPIO.output(self.gpio, GPIO.LOW)
-        print(">> Current GPIO state:")
         print(GPIO.input(self.gpio))
