ebb1bddf22a7a311a4a5db734a325bca75bd8133
husam <husam6101@gmail.com>
2024-04-28 02:37:27 +0300

    Refactor import statements in tests.py

--- test/tests.py
+++ test/tests.py
@@ -1,8 +1,6 @@
 from pi_voice.operators import logger
-logger.debug("Importing...")
-from pi_voice.main_multiprocess import MainProcess
-logger.debug("Imported!")
-logger.debug("Starting...")
+
+import pi_voice.main_multiprocess
 
 if __name__ == "__main__":
-    MainProcess().start()
+    pi_voice.main_multiprocess.MainProcess().start()
