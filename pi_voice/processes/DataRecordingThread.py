3049cc1d807c91695def3996da00eb52d78f2666
husam <husam6101@gmail.com>
2024-04-29 00:29:18 +0300

    fixed data process not working

--- pi_voice/processes/DataRecordingThread.py
+++ pi_voice/processes/DataRecordingThread.py
@@ -9,78 +9,76 @@ from pi_voice.processes.ErrorHandling import ErrorSeverity
 from pi_voice.switcher.SensorSwitcher import SensorSwitcher
 from pi_voice.utils.common import get_time_of_day_and_day_of_week, retry_on_exception
 from pi_voice.utils.synchronization import writing_lgbm_data
-
-import time
+from pi_voice import logger
 
 
 class DataRecordingThread:
     def __init__(
         self,
         sensor_switcher: SensorSwitcher,
-        gpt2_pipe,
-        action_prediction_finished_event: Event,
+        data_queue: Queue,
         error_queue: Queue,
         stop_flag: Event,
         active_processes_count: Synchronized,
     ):
         self.data_op: DataOperator = DataOperator()
         self.sensor_switcher: SensorSwitcher = sensor_switcher
-        self.gpt2_pipe = gpt2_pipe
-        self.action_prediction_finished_event: Event = action_prediction_finished_event
+        self.data_queue: Queue = data_queue
         self.error_queue: Queue = error_queue
         self.stop_flag: Event = stop_flag
         self.active_processes_count: Synchronized = active_processes_count
 
-    def get_data(self, action):
-        try:
-            logger.info("Fetching data from sensors...")
-            temp, humid, light = retry_on_exception(self.sensor_switcher.get_data)
-            time_of_day, day_of_week = get_time_of_day_and_day_of_week()
-            logger.info("Done.")
-            logger.info(
-                "Data to be written to file: "
-                + str([temp, humid, light, time_of_day, day_of_week, action])
-            )
-            return [temp, humid, light, time_of_day, day_of_week, action]
-        except Exception as e:
-            self.error_queue.put((str(e), "device_errors", ErrorSeverity.HIGH))
-
-    def run(self, action):
+    def run(self):
         self.active_processes_count.value += 1
 
         while True:
-            if self.action_prediction_finished_event.wait(timeout=4):
-                try:
-                    self.gpt2_pipe.recv()
-                    
-                    data = self._get_data(action)
+            try:
+                action = self.data_queue.get(block=True, timeout=4)
+                logger.info("Received action. Fetching data...")
+                
+                data = self._get_data(action)
+                logger.info("Data fetched. Writing data to file...")
 
-# continue unencapsulating exceptions
-                    try:
-                        logger.info("Writing data file...")
-                        writing_lgbm_data.acquire(timeout=2.0)
-                        retry_on_exception(
-                            self.data_op.add_row_to_csv,
-                            (
-                                get_path_from(config["lgbm"]["dataset"]),
-                                data,
-                            ),
-                        )
-                        writing_lgbm_data.release()
-                        logger.info("Done.")
-                    except Exception as e:
-                        self.error_queue.put((str(e), "io_errors", ErrorSeverity.HIGH))
-                        continue
+                self._write_data_to_file(data)
+                logger.info("Done writing data to file.")
+            except Exception as e:
+                self.error_queue.put((str(e), "io_errors", ErrorSeverity.LOW))
+                continue
 
-                except Exception as e:
-                    self.error_queue.put((str(e), "process_errors", ErrorSeverity.LOW))
-                    continue
-                finally:
-                    self.action_prediction_finished_event.clear()
-
-                if self.stop_flag.is_set():
-                    self.active_processes_count.value -= 1
-                    break
-            elif self.stop_flag.is_set():
+            if self.stop_flag.is_set():
                 self.active_processes_count.value -= 1
                 break
+
+    def _get_data(self, action):
+        try:
+            logger.debug("Fetching data from sensors...")
+            temp, humid, light = retry_on_exception(self.sensor_switcher.get_data)
+            time_of_day, day_of_week = get_time_of_day_and_day_of_week()
+            logger.debug("Done fetching data.")
+            
+            data = [temp, humid, light, time_of_day, day_of_week, action]
+            logger.debug(
+                "Data to be written to file: "
+                + str(data)
+            )
+            
+            return data
+        except Exception as e:
+            self.error_queue.put((str(e), "device_errors", ErrorSeverity.HIGH))
+
+    def _write_data_to_file(self, data):
+        try:
+            logger.debug("Securing thread and writing data file...")
+            writing_lgbm_data.acquire(timeout=2.0)
+            retry_on_exception(
+                self.data_op.add_row_to_csv,
+                (
+                    get_path_from(config["lgbm"]["dataset"]),
+                    data,
+                ),
+            )
+            writing_lgbm_data.release()
+            logger.debug("Done writing data file.")
+        except Exception as e:
+            self.error_queue.put((str(e), "io_errors", ErrorSeverity.HIGH))
+            
--- pi_voice/processes/PipeToThreadQueuesManagerThread.py
+++ pi_voice/processes/PipeToThreadQueuesManagerThread.py
@@ -8,32 +8,27 @@ class PipeToThreadQueuesManagerThread:
     def __init__(
         self,
         pipe,
-        action_prediction_finished_event: Event,
-        # p2q_sent_event: Event,
+        process_event: Event,
         thread_queues: list[q],
     ):
         self.pipe = pipe
-        self.action_prediction_finished_event: Event = action_prediction_finished_event
-        # self.p2q_sent_event: Event = p2q_sent_event
+        self.process_event: Event = process_event
         self.thread_queues: list[q] = thread_queues
 
     def _pipe_data_to_threads(self):
         while True:
-            if self.action_prediction_finished_event.wait(timeout=3):
+            if self.process_event.wait(timeout=3):
                 try:
                     data = self.pipe.recv()
                     logger.info(f"Received data: {data}")
                     
                     logger.info("Queueing data to threads...")
                     for queue in self.thread_queues:
-                        print(f"Queue: {queue}")
                         queue.put(data)
-
-                    # self.p2q_sent_event.set()
                 except Exception as e:
                     logger.error(f"Error piping data to threads: {e}")
                 finally:
-                    self.action_prediction_finished_event.clear()
+                    self.process_event.clear()
 
     def run(self):
         self._pipe_data_to_threads()
